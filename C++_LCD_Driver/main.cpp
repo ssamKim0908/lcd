eef23de1418542957ccfc65538f0b7428ae4d7ad
ssamkim0908 <mmmek0401@gmail.com>
2026-04-29 01:20:46 +0900

    test code in main

--- C++_LCD_Driver/main.cpp
+++ C++_LCD_Driver/main.cpp
@@ -3,6 +3,7 @@
 #include "hal/gpio.hpp"
 #include "display/LcdWriter.hpp"
 #include "display/St7789Lcd.hpp"
+#include "input/GpioKeys.hpp"
 #include "util/time.hpp"
 #include "util/color.hpp"
 
@@ -18,11 +19,25 @@ int main()
     auto writer = std::make_unique<LcdWriter>(std::move(spi), std::move(gw));
     auto lcd    = std::make_unique<St7789Lcd>(std::move(writer));
 
+    auto input  = std::make_unique<GpioRead>(chip->get_fd());
+    auto key    = std::make_unique<GpioKeys>(std::move(input));
+
     lcd->init();
 
-    lcd->clear(RED);   sleep_ms(1000);
-    lcd->clear(GREEN); sleep_ms(1000);
-    lcd->clear(BLUE);  sleep_ms(1000);
+    auto c = std::vector<uint16_t>({RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA});
 
+    int cur = 1;
+    while(cur > 0)
+    {
+        lcd->clear(c[cur]);
+        auto k = key->next_event();
+        if(k.pressed)
+        {
+            switch(k.key)
+            {
+            case Key::Left : cur = cur > 0 ? cur - 1 : cur; break;
+            case Key::Right: cur = cur < c.size() - 1 ? cur + 1 : cur; break;
+            }
+        }
+    }
     return 0;
-}
